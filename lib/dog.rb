20d169d4ff525b6eb8be5254c34d34898a4f6dfa
Ming Duan <11203duan@gmail.com>
2019-05-03 03:00:55 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -72,7 +72,7 @@ def self.new_from_db(row)
   self.new(id: row[0], name: row[1], breed: row[2])
 end
 
-def self.find_by_name
+def self.find_by_name(name)
   sql = <<-SQL
   SELECT * FROM dogs WHERE name = ?
   SQL
