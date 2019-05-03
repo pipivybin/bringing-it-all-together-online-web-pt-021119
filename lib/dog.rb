a9ea26fee4bbce6221ac6e50001e70baa777835c
Ming Duan <11203duan@gmail.com>
2019-05-03 02:07:14 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -61,7 +61,7 @@ def self.find_or_create_by(name:, breed:)
   SELECT * FROM dogs WHERE name = ? AND breed = ?
   SQL
 
-  result = DB[:conn].execute(sql, name, breed)[0]
+  result = DB[:conn].execute(sql, name, breed)
 binding.pry
   if result.empty?
 
