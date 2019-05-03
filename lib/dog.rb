150bb42581e8661237938ca0be16551274efb630
Ming Duan <11203duan@gmail.com>
2019-05-03 01:56:30 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -65,6 +65,7 @@ def self.find_or_create_by(name:, breed:)
   if result.empty?
     self.create(name: result[0], breed: result[1])
   else
+    self.new(id: result[0], name: result[1], breed: result[2])
   end
 
 end
